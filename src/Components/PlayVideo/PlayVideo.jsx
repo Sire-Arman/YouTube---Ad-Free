1bd0c9c2d2c5acb42fdef5aed1c1fde3537b4366
Arman Siddiqui <arman2113022@akgec.ac.in>
2024-06-10 01:24:49 +0530

    autoplay on

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -69,7 +69,7 @@ function PlayVideo() {
       <iframe
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
         frameBorder="0"
-        allow="accelerometer; {autoplay;} clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
